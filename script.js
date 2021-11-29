1104c9020ac8790b6f7a227eb2c9d93deb68bcf6
chojak <chojecki2000@gmail.com>
2021-11-29 12:48:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var productList = [
+    {
+        name: "Chleb",
+        amount: 5,
+        price: 2.5,
+    },
+    {
+        name: "Denaturat",
+        amount: 1,
+        price: 5.3,
+    },
+    {
+        name: "Stern",
+        amoutn: 3,
+        price: 2.99,
+    },
+]
+
+function printList() {
+    let table = document.getElementById("paragon");
+    printList.array.forEach(element => {
+        
+    });
+}
